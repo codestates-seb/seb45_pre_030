dedad9fff7c187045041b6c0c2e7fe05161064dd
seonhoshin <shinwder@gmail.com>
2023-08-09 15:41:16 +0900

    first

---
+++ server/src/test/java/com/codestates/StackOverFlowClone/StackOverFlowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.StackOverFlowClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
