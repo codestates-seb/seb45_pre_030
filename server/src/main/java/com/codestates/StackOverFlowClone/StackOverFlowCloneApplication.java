22fa7d92a0dbdc7163a3f42fe1be00c252de94c1
seonhoshin <shinwder@gmail.com>
2023-08-09 17:20:54 +0900

    be-init 브런치 생성

--- server/src/main/java/com/codestates/StackOverFlowClone/StackOverFlowCloneApplication.java
+++ server/src/main/java/com/codestates/StackOverFlowClone/StackOverFlowCloneApplication.java
@@ -2,12 +2,19 @@ package com.codestates.StackOverFlowClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StackOverFlowCloneApplication {
+public class StackOverFlowCloneApplication   extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StackOverFlowCloneApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StackOverFlowCloneApplication.class);
 	}
 
 }
