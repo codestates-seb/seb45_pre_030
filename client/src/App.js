8b96d1e57aa9be849aae4806d9b8b15c827e1843
dice0314 <dice0314@naver.com>
2023-08-09 16:04:47 +0900

    feat:FE초기세팅

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>초기화면</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
