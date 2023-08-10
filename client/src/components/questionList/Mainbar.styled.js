2fa8df96c6f1fdbd7e506dc4fca7789bf16d6820
dice0314 <dice0314@naver.com>
2023-08-10 17:56:23 +0900

    feat: 질문리스트 기본틀

---
+++ client/src/components/questionList/Mainbar.styled.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  height: 200px;
+  width: 700px;
+`;
+
+export const StatsContainer = styled.div`
+  width: 50px;
+  background-color: #fff;
+`;
+
+export const ContentContainer = styled.div`
+  background-color: green;
+  height: 100px;
+  width: 500px;
+`;
+
+export const StatsItem = styled.p`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 14px;
+  height: 10px;
+`;
+
+export const Title = styled.h2`
+  background-color: gray;
+`;
+
+export const Content = styled.p`
+  display: flex;
+  align-items: center;
+  background-color: gray;
+`;
+
+export const MetaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: yellow;
+  height: 50px;
+  width: 500px;
+`;
+
+export const TagContainer = styled.ul`
+  background-color: skyblue;
+  height: 30px;
+  width: 300px;
+`;
