fc85b0b6e475c73456f4b2cde27d1ea42978a75a
KimYMin <minmin4439@naver.com>
2023-08-11 13:39:21 +0900

    footer 구현

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,25 @@
-import { Container } from "./Footer.styled";
-
-
-function Footer(){
-
-    return(
-        <Container>
-            <Text>안녕</Text>
-        </Container>
-    )
-
+import { Container, LogoContainer, Logo, FooterNav } from './Footer.styled';
+import LogoSrc from '../../common/image/icon.png';
+import StackOverflow from './FooterStack';
+import Products from './FooterProducts';
+import Company from './FooterCompany';
+import Network from './FooterNetwork';
+import Copyright from './FooterCopyright';
+function Footer() {
+  return (
+    <Container>
+      <LogoContainer>
+        <Logo src={LogoSrc} />
+      </LogoContainer>
+      <FooterNav>
+        <StackOverflow />
+        <Products />
+        <Company />
+        <Network />
+      </FooterNav>
+      <Copyright />
+    </Container>
+  );
 }
 
 export default Footer;
