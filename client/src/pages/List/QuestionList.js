70bf67c2eb494374048009217326fd640ee418f3
dice0314 <dice0314@naver.com>
2023-08-11 14:16:59 +0900

    feat: 질문 컴포넌트 틀 완성

---
+++ client/src/pages/List/QuestionList.js
@@ -0,0 +1,16 @@
+import Mainbar from '../../components/questionList/Mainbar';
+import { Title } from './QuestionList.styled';
+
+function QuestionList() {
+  return (
+    <div>
+      <Title>All Questions</Title>
+      <Mainbar />
+      <Mainbar />
+      <Mainbar />
+      <Mainbar />
+    </div>
+  );
+}
+
+export default QuestionList;
