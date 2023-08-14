08ec8d9e45861cb99e82e062f618f2854b63450b
dice0314 <dice0314@naver.com>
2023-08-14 18:12:31 +0900

    feat: 사이드 위젯 생성 1차

---
+++ client/src/components/questionList/CollectivesWidget/CollectivesWidget.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.ul`
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  overflow: hidden;
+  background-color: hsl(210, 8%, 97.5%);
+  border: 1px solid hsl(210, 8%, 85%);
+  width: 100%;
+  height: auto;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 15px;
+`;
+
+export const Text = styled.p`
+  font-size: 13px;
+`;
+
+export const Hyper = styled.a`
+  font-size: 11px;
+  color: hsl(206, 100%, 40%);
+`;
+
+export const Content = styled.div``;
+
+export const ContentWrapper = styled.div``;
