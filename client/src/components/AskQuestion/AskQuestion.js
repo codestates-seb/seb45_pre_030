4f864987fe00a7f5010530502825c68c6c0282e2
KimYMin <minmin4439@naver.com>
2023-08-16 13:39:51 +0900

    feat: 질문 생성 시 id값 추가

--- client/src/components/AskQuestion/AskQuestion.js
+++ client/src/components/AskQuestion/AskQuestion.js
@@ -2,11 +2,15 @@ import { QuestionContainer, Headline } from './AQ.styled';
 import Manual from './AQManual';
 import Title from './AQTitle';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function AskQuestion() {
   // 질문 데이터
   const [data, setData] = useState([]);
+
+  // 질문 생성시 id값 1씩 증가
+  const dataId = useRef(0);
+
   // 질문 생성
   const onCreate = (title, problem) => {
     const date = new Date().getTime();
@@ -14,7 +18,9 @@ function AskQuestion() {
       title,
       problem,
       date,
+      id: dataId.current,
     };
+    dataId.current++;
     setData([newItem, ...data]);
   };
 
