a025ad4419cb0d3f8af62c296bf9ac13e5220eba
seonhoshin <shinwder@gmail.com>
2023-08-16 17:05:30 +0900

    feat show comments

--- server/src/main/java/com/codestates/StackOverFlowClone/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/codestates/StackOverFlowClone/question/mapper/QuestionMapper.java
@@ -1,6 +1,8 @@
 package com.codestates.StackOverFlowClone.question.mapper;
 
 
+import com.codestates.StackOverFlowClone.comment.dto.CommentResponseDto;
+import com.codestates.StackOverFlowClone.comment.entity.Comment;
 import com.codestates.StackOverFlowClone.question.dto.QuestionDto;
 import com.codestates.StackOverFlowClone.question.dto.OnlyQuestionResponseDto;
 import com.codestates.StackOverFlowClone.question.entity.Question;
@@ -27,6 +29,7 @@ public interface QuestionMapper {
                         .viewCount(question.getViewCount())
                         .build();
         List<ReplyResponseDto> replyResponseDtos = new ArrayList<>();
+
         if(question.getReplies() != null) {
             for(Reply reply : question.getReplies()) {
                 ReplyResponseDto replyResponseDto =
@@ -36,10 +39,29 @@ public interface QuestionMapper {
                                 .memberId(reply.getMemberId())
                                 .content(reply.getContent())
                                 .createdAt(reply.getCreatedAt())
-                                .comments(reply.getComments())
                                 .build();
+
+                List<CommentResponseDto> commentResponseDtos = new ArrayList<>();
+
+                if(reply.getComments() != null){
+                    for(Comment comment : reply.getComments()){
+                        CommentResponseDto commentResponseDto =
+                                CommentResponseDto.builder()
+                                        .commentId(comment.getCommentId())
+                                        .content(comment.getContent())
+                                        .createdAt(comment.getCreatedAt())
+                                        .replyId(comment.getReply().getReplyId())
+                                        .build();
+
+                        commentResponseDtos.add(commentResponseDto);
+                    }
+                }
+
+                replyResponseDto.setComments(commentResponseDtos);
+
                 replyResponseDtos.add(replyResponseDto);
             }
+
         }
         return new QuestionDto.Response(onlyQuestionResponseDto, replyResponseDtos);
     }
@@ -49,70 +71,3 @@ public interface QuestionMapper {
     List<OnlyQuestionResponseDto> questionsToOnlyQuestionResponseDtos(List<Question> questions);
 
 }
-
-
-//public interface QuestionMapper {
-//
-//    default Content contentPostDtoToContent(ContentPostDto contentPostDto){
-//        Content content = new Content();
-//
-//        content.setTitle(contentPostDto.getTitle());
-//        content.setContent(contentPostDto.getContent());
-//        content.setAttachFile(contentPostDto.getAttachFile());
-//        Member member = new Member();
-//        member.setMemberId(contentPostDto.getMemberId());
-//        member.setName(contentPostDto.getMemberName());
-//        content.setMember(member);
-//        Category category = new Category();
-//        category.setCategoryId(contentPostDto.getCategoryId());
-//        category.setCategoryName(contentPostDto.getCategoryName());
-//        content.setCategory(category);
-//        // 만약 content.createdAt 빼서 안나올 경우... 수동으로 넣어.
-////        content.setCreatedAt(LocalDateTime.now());
-////        content.setModifiedAt(LocalDateTime.now());
-//
-//        return content;
-//
-//    }
-//
-//    default Content contentPatchDtoToContent(ContentPatchDto contentPatchDto){
-//
-//        Content content = new Content();
-//
-//        content.setNoticeId(contentPatchDto.getNoticeId());
-//        content.setTitle(contentPatchDto.getTitle());
-//        content.setContent(contentPatchDto.getContent());
-//        content.setAttachFile(contentPatchDto.getAttachFile());
-//        Category category = new Category();
-//        category.setCategoryId(contentPatchDto.getCategoryId());
-//        category.setCategoryName(contentPatchDto.getCategoryName());
-//        content.setCategory(category);
-//        Member member = new Member();
-//        member.setMemberId(contentPatchDto.getMemberId());
-//        member.setName(contentPatchDto.getMemberName());
-//        content.setMember(member);
-//        // 만약 content.createdAt 빼서 안나올 경우... 수동으로 넣어.
-////        content.setCreatedAt(LocalDateTime.now());
-////        content.setModifiedAt(LocalDateTime.now());
-//
-//        return content;
-//    }
-//
-//    default ContentResponseDto contentToContentResponseDto(Content content){
-//        ContentResponseDto contentResponseDto = new ContentResponseDto();
-//
-//        contentResponseDto.setNoticeId(content.getNoticeId());
-//        contentResponseDto.setTitle(content.getTitle());
-//        contentResponseDto.setContent(content.getContent());
-//        contentResponseDto.setAttachFile(content.getAttachFile());
-//        contentResponseDto.setCategoryId(content.getCategory().getCategoryId());
-//        contentResponseDto.setMemberId(content.getMember().getMemberId());
-//        contentResponseDto.setCategoryName(content.getCategory().getCategoryName());
-//        contentResponseDto.setMemberName(content.getMember().getName());
-//        contentResponseDto.setCreatedAt(content.getCreatedAt());
-//        contentResponseDto.setModifiedAt(content.getModifiedAt());
-//
-//        return contentResponseDto;
-//    }
-//
-//}
