8fb83557994e1ca6816b666fea54ed7b9459f8b7
dice0314 <dice0314@naver.com>
2023-08-16 17:00:42 +0900

    feat: 리베이스 작업

--- client/src/components/questionList/Mainbar.js
+++ client/src/components/questionList/Mainbar.js
@@ -5,23 +5,41 @@ import {
   MetaContainer,
   StatsContainer,
   StatsItem,
+  Tag,
   TagContainer,
+  Time,
   Title,
+  UserCardContainer,
+  UserImg,
+  UserInfo,
 } from './Mainbar.styled';
 
 function Mainbar() {
   return (
     <Container>
       <StatsContainer>
-        <StatsItem>votes</StatsItem>
-        <StatsItem>answers</StatsItem>
-        <StatsItem>views</StatsItem>
+        <StatsItem>0 votes</StatsItem>
+        <StatsItem>0 answers</StatsItem>
+        <StatsItem>2 views</StatsItem>
       </StatsContainer>
       <ContentContainer>
         <Title>Coursera React lab cant compile React code</Title>
-        <Content>{`Coursera lab can't compile the react code, function App() { function handleClick() { let randomNum = Math.floor(Math.random() * 3) + 1;`}</Content>
+        <Content>{`https://imanudin.net/2020/10/17/zimbra-tips-how-to-add-external-email-warning-message/ I have used this guide to set up my caution message but it doesn't seem to`}</Content>
         <MetaContainer>
-          <TagContainer></TagContainer>
+          <TagContainer>
+            <Tag type="button" value="sql" />
+            <Tag type="button" value="javascript" />
+            <Tag type="button" value="html" />
+            <Tag type="button" value="css" />
+          </TagContainer>
+          <UserCardContainer>
+            <UserImg src="logo192.png" />
+            <UserInfo>
+              <a href="/">hermit</a>
+              <p>573</p>
+            </UserInfo>
+            <Time>asked 55 secs ago</Time>
+          </UserCardContainer>
         </MetaContainer>
       </ContentContainer>
     </Container>
