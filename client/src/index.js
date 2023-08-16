563b5cfb2d535a2f58d50c8089f8161b7b2fd77f
dice0314 <dice0314@naver.com>
2023-08-16 15:07:34 +0900

    feat: Questions페이지 리베이스

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { GlobalStyle } from './style/globalStyle';
+import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux';
 
