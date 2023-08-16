dcb286451b5c3bf1a1a5c63e156a8e1e78acfdd6
hjwnu <tlstjr01@gmail.com>
2023-08-16 17:42:18 +0900

    feat/ get Reply<->Comment

--- server/src/main/java/com/codestates/StackOverFlowClone/reply/mapper/ReplyMapper.java
+++ server/src/main/java/com/codestates/StackOverFlowClone/reply/mapper/ReplyMapper.java
@@ -1,16 +1,14 @@
 package com.codestates.StackOverFlowClone.reply.mapper;
 
+import com.codestates.StackOverFlowClone.comment.dto.CommentResponseDto;
+import com.codestates.StackOverFlowClone.comment.entity.Comment;
 import com.codestates.StackOverFlowClone.question.entity.Question;
 import com.codestates.StackOverFlowClone.reply.dto.ReplyDto;
-import com.codestates.StackOverFlowClone.reply.dto.ReplyPatchDto;
-import com.codestates.StackOverFlowClone.reply.dto.ReplyPostDto;
 import com.codestates.StackOverFlowClone.reply.dto.ReplyResponseDto;
 import com.codestates.StackOverFlowClone.reply.entity.Reply;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 @Mapper(componentModel = "spring")
@@ -20,41 +18,9 @@ public interface ReplyMapper {
     Reply ReplyPostDtoToReply(ReplyDto.Post replyPostDto);
     @Mapping(source = "questionId", target = "question.questionId")
     Reply ReplyPatchDtoToReply(ReplyDto.Patch replyPatchDto);
-    @Mapping(source = "question.questionId", target = "questionId")
-    ReplyResponseDto ReplyToReplyResponseDto(Reply reply);
-    List<ReplyResponseDto> RepliesToReplyResponseDtos(List<Reply> replies);
-
-/*
-    @Override
-    default public Reply ReplyPostDtoToReply(ReplyDto.Post replyPostDto) {
-        if ( replyPostDto == null ) {
-            return null;
-        }
-
-        Reply reply = new Reply();
-
-        reply.setQuestion( postToQuestion( replyPostDto ) );
-        reply.setMemberId( replyPostDto.getMemberId() );
-        reply.setContent( replyPostDto.getContent() );
-
-        return reply;
-    }
-
-
-    default public Reply ReplyPatchDtoToReply(ReplyDto.Patch replyPatchDto) {
-        if ( replyPatchDto == null ) {
-            return null;
-        }
-
-        Reply reply = new Reply();
-
-        reply.setQuestion( patchToQuestion( replyPatchDto ) );
-        reply.setReplyId( replyPatchDto.getReplyId() );
-        reply.setMemberId( replyPatchDto.getMemberId() );
-        reply.setContent( replyPatchDto.getContent() );
-
-        return reply;
-    }
+//    @Mapping(source = "question.questionId", target = "questionId")
+//    ReplyResponseDto ReplyToReplyResponseDto(Reply reply);
+//    List<ReplyResponseDto> RepliesToReplyResponseDtos(List<Reply> replies);
 
 
     default public ReplyResponseDto ReplyToReplyResponseDto(Reply reply) {
@@ -69,45 +35,22 @@ public interface ReplyMapper {
         replyResponseDto.memberId( reply.getMemberId() );
         replyResponseDto.content( reply.getContent() );
         replyResponseDto.createdAt( reply.getCreatedAt() );
+        replyResponseDto.comments( commentListToCommentResponseDtoList( reply.getComments() ) );
 
         return replyResponseDto.build();
     }
 
-    public List<ReplyResponseDto> RepliesToReplyResponseDtos(List<Reply> replies) {
+    default public List<ReplyResponseDto> RepliesToReplyResponseDtos(List<Reply> replies) {
         if ( replies == null ) {
             return null;
         }
 
-        List<ReplyResponseDto> list = new ArrayList<ReplyResponseDto>( replies.size() );
+        List<ReplyResponseDto> list = new ArrayList<>( replies.size() );
         for ( Reply reply : replies ) {
             list.add( ReplyToReplyResponseDto( reply ) );
         }
 
         return list;
-    }
-
-    protected Question postToQuestion(ReplyDto.Post post) {
-        if ( post == null ) {
-            return null;
-        }
-
-        Question question = new Question();
-
-        question.setQuestionId( post.getQuestionId() );
-
-        return question;
-    }
-
-    protected Question patchToQuestion(ReplyDto.Patch patch) {
-        if ( patch == null ) {
-            return null;
-        }
-
-        Question question = new Question();
-
-        question.setQuestionId( patch.getQuestionId() );
-
-        return question;
     }
 
     private long replyQuestionQuestionId(Reply reply) {
@@ -121,6 +64,33 @@ public interface ReplyMapper {
         long questionId = question.getQuestionId();
         return questionId;
     }
-    */
+
+    default CommentResponseDto commentToCommentResponseDto(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentResponseDto.CommentResponseDtoBuilder commentResponseDto = CommentResponseDto.builder();
+
+        commentResponseDto.commentId( comment.getCommentId() );
+        commentResponseDto.content( comment.getContent() );
+        commentResponseDto.replyId(comment.getReply().getReplyId());
+        commentResponseDto.createdAt( comment.getCreatedAt() );
+
+        return commentResponseDto.build();
+    }
+
+    default List<CommentResponseDto> commentListToCommentResponseDtoList(List<Comment> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<CommentResponseDto> list1 = new ArrayList<>( list.size() );
+        for ( Comment comment : list ) {
+            list1.add( commentToCommentResponseDto( comment ) );
+        }
+
+        return list1;
+    }
 
 }
