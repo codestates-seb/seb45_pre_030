65e38b84475062c350816da1d2932775708af2ce
lse0522 <88942643+lse0522@users.noreply.github.com>
2023-08-18 00:24:13 +0900

    login/스타일 변경

--- client/src/components/Login/Login.styled.js
+++ client/src/components/Login/Login.styled.js
@@ -1,50 +1,62 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+export const Logo = styled.img`
+  width: 50px;
+  margin: 0 auto;
+  margin-top: 40px;
+`;
+
 export const From = styled.label`
-  width: 600px;
+  width: 400px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   box-shadow: 0 0 5px gray;
   border-radius: 10px;
   margin-top: 40px;
-`;
-
-export const Head = styled.h1`
-  margin: 40px;
-  font-weight: 900;
-  font-size: 20px;
+  padding: 40px;
 `;
 
 export const Label = styled.h4`
-  margin-left: 40px;
   float: left;
-  font-weight: 500;
+  font-weight: 800;
   margin-bottom: 10px;
 `;
 
 export const Input = styled.input`
-  margin-left: 40px;
-  width: 80%;
+  width: 100%;
   height: 30px;
   margin-bottom: 30px;
+  border-radius: 4px;
+  border: 1px solid gray;
 `;
 
 export const Button = styled.button`
-  margin-left: 40px;
-  width: 82%;
+  width: 100%;
   height: 40px;
-  margin: 20px 40px;
   background: #0a95ff;
   border-radius: 10px;
-  font-weight: 600;
   color: white;
 `;
 
-export const StyledNav = styled(Link)`
-  /* margin: 0 auto; */
-  margin: 0px 40px 40px 262px;
-  color: gray;
+export const EmailText = styled.span`
   font-size: 14px;
+  font-weight: 600;
+  color: #de5054;
+  display: ${(props) => (props.emailText === 'null' ? 'null' : 'none')};
+  position: fixed;
+  top: 286px;
+`;
+
+export const StyledNav = styled(Link)`
+  color: #0074cc;
+  font-size: 14px;
+  margin: 0 auto;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`;
+
+export const Text = styled.text`
+  color: black;
 `;
