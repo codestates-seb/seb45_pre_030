96d12fe12224875a3d395a629ac16692b6beb419
hjwnu <tlstjr01@gmail.com>
2023-08-17 23:34:19 +0900

    fix / wrong parameter

--- server/src/main/java/com/codestates/StackOverFlowClone/comment/controller/CommentController.java
+++ server/src/main/java/com/codestates/StackOverFlowClone/comment/controller/CommentController.java
@@ -43,7 +43,7 @@ public class CommentController {
         return ResponseEntity.created(location).build();
     }
 
-    @PatchMapping("{comment-id}")gg
+    @PatchMapping("{comment-id}")
     public ResponseEntity patchComment(@Valid @RequestBody CommentDto.Patch requestBody, @PathVariable("reply-id") @Positive long replyId,
                                        @PathVariable("comment-id") @Positive long commentId) {
         requestBody.setCommentId(commentId);
