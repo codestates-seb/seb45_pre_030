7836a748a306de4f93e8b533584b761527401f4a
dice0314 <dice0314@naver.com>
2023-08-22 13:52:46 +0900

    fix: 중복 오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Footer from './components/Footer/Footer';
 import AskQuestion from './components/AskQuestion/AskQuestion';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
 import QuestionDetail from './pages/QuestionDetail/QuestionDetail';
 import Signup from './components/Signup/Signup/Signup';
 import Profile from './components/Profile/Profile';
