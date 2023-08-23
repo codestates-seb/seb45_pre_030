e0ec25c47453b43659c8f7f0914868d210bcd0be
seonhoshin <shinwder@gmail.com>
2023-08-23 11:47:12 +0900

    server deleted

--- server/src/main/java/com/codestates/StackOverFlowClone/question/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.StackOverFlowClone.question.repository;
-
-import com.codestates.StackOverFlowClone.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
